af38efa29c39647967e636c9a0865355fc22ddf3
jedel <jedeland@live.co.uk>
2020-01-07 15:15:43 +0100

    Using pandas and tensor to analyse weather
    Project case study found in Fabio Nellis Python Data Analysis

--- venv/dataan_po_valley.py
+++ venv/dataan_po_valley.py
@@ -1,5 +1,13 @@
 import numpy as np; import pandas as pd
 import datetime
+import json; import requests
+#Initialise this file by import pandas, using pip install x
+#This program aims to analyse the links between weather data, relating to the adriatic ocean and the po valley
+#Initialises JSON data to city name
+ferrara = json.loads(requests
+                     .get('http://api.openweathermap.org/data/2.5/'
+                          '?q=Ferrara,IT&appid=5807ad2a45eb6bf4e81d137dafe74e15').text)
+print(ferrara)
 
 
 
